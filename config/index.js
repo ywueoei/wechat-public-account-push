caaa66510f2dd775926fa4122ed972c005c3010c
ywueoei <35222482+ywueoei@users.noreply.github.com>
2022-09-17 22:41:34 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -65,7 +65,7 @@ export const config = {
       id: "oyuVB59blP2BksYvdIoBS-6Ek0YU",
 //       id: "oyuVB5_76raBcZXXwZ5-vbtPzRMg",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Wf5bxU5-oT4tGn5VjY1lkyaJZ7bZTxi-C2Hyn69sHHg",
+      useTemplateId: "E1gdzSHh2iHlUDDZdMpi-wcV9Br_pLI269NhV8gpXJE",
       // 所在省份
       province: "陕西",
       // 所在城市
