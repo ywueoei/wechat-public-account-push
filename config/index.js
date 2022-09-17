b7cb810a1e0cf251138a7e93ed17235ba3f9f591
ywueoei <35222482+ywueoei@users.noreply.github.com>
2022-09-17 18:26:13 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -63,7 +63,42 @@ export const config = {
       name: "老婆姐姐",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oyuVB59blP2BksYvdIoBS-6Ek0YU",
-//       id: "oyuVB5_76raBcZXXwZ5-vbtPzRMg"
+//       id: "oyuVB5_76raBcZXXwZ5-vbtPzRMg",
+      // 你想对他发送的模板消息的模板ID
+      useTemplateId: "CNFEykrRl92jKHnJbcb6QPyF-gVHnrv6IXJjYn1rwiE",
+      // 所在省份
+      province: "陕西",
+      // 所在城市
+      city: "渭南",
+      // 新历生日, 仅用作获取星座运势, 格式必须
+      horoscopeDate: '11-07',
+      // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
+      horoscopeDateType: '今日',
+      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
+      openUrl: "https://wangxinleo.cn",
+      // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {"type": "*生日", "name": "美丽可爱亲亲老婆", "year": "1996", "date": "09-27"},
+        {"type": "节日", "name": "与猪猪老婆相拥", "year": "2020", "date": "10-01"},
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+      ],
+      // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
+      customizedDateList: [
+        // 在一起的日子
+        {"keyword": "love_day", date: "2021-04-23"},
+        // 结婚纪念日--一见钟情
+        {"keyword": "marry_day", date: "2014-06-04"},
+        // 退伍日
+//         {"keyword": "ex_day", date: "2022-09-10"},
+      ]
+    },
+    {
+      // 想要发送的人的名字
+      name: "老婆姐姐",
+      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+//       id: "oyuVB59blP2BksYvdIoBS-6Ek0YU",
+      id: "oyuVB5_76raBcZXXwZ5-vbtPzRMg",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "CNFEykrRl92jKHnJbcb6QPyF-gVHnrv6IXJjYn1rwiE",
       // 所在省份
